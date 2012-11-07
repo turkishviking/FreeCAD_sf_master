7cdae473ec015a5792cda308d9ae7870c2103498
arobinson <arobinson@freecad>
2012-11-07 22:01:08 +1100

    Added ToolPath Dock Window. Currently not connected to anything.

--- src/Mod/Cam2/Gui/Workbench.cpp
+++ src/Mod/Cam2/Gui/Workbench.cpp
@@ -28,8 +28,12 @@
 #endif
 
 #include "Workbench.h"
+#include <Gui/MainWindow.h>
 #include <Gui/MenuManager.h>
 #include <Gui/ToolBarManager.h>
+#include <Gui/DockWindowManager.h>
+
+#include "ToolPathDockWindow.h"
 
 using namespace CamGui;
 
@@ -38,6 +42,14 @@ TYPESYSTEM_SOURCE(CamGui::Workbench, Gui::StdWorkbench)
 
 Workbench::Workbench()
 {
+  Gui::DockWindowManager* pDockMgr = Gui::DockWindowManager::instance();
+  Gui::MainWindow* pMainWindow = Gui::MainWindow::getInstance();
+
+  // Add Toolpath Dock Window
+  ToolPathDockWindow* pcTPView = new ToolPathDockWindow(0, pMainWindow);
+  pcTPView->setObjectName("Tool Path");
+  pcTPView->setMinimumWidth(150);
+  pDockMgr->registerDockWindow("Cam_ToolPathDockWindow", pcTPView);
 }
 
 Workbench::~Workbench()
@@ -74,3 +86,13 @@ Gui::ToolBarItem* Workbench::setupCommandBars() const
     return root;
 }
 
+
+Gui::DockWindowItems* Workbench::setupDockWindows() const
+{
+    Gui::DockWindowItems* root = Gui::StdWorkbench::setupDockWindows();
+    root->addDockWidget("Cam_ToolPathDockWindow", Qt::BottomDockWidgetArea, true, false);
+//    root->setVisibility(false); // hide all dock windows by default
+//    root->setVisibility("Std_CombiView",true); // except of the combi view
+//    root->setVisibility("Cam_ToolPathDockWindow",true); // Show ToolPathDockWindow
+    return root;
+}
